9d42f8c4c85b12d65ff17dca093fa066863c3461
renaerdelyi <rena.szabo@yahoo.com>
2025-03-10 06:30:33 +0200

    add5

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,11 @@ import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Movies from './pages/Movies/Movies';
 import MovieDetails from './components/MovieDetails/MovieDetails';
-import NotFoundPage from 'pages/common/NotFound/NotFoundPage';
+// import NotFoundPage from 'pages/common/NotFound/NotFoundPage';
 
 const Cast = React.lazy(() => import('./components/MovieDetails/Cast'));
 const Reviews = React.lazy(() => import('./components/MovieDetails/Reviews'));
+const NotFoundPage = React.lazy(() => import('./pages/common/NotFound/NotFoundPage'))
 
 const App = () => {
   return (
