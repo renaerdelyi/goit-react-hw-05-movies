1b0f6b667919d535019b2ef9509e5a86436728b0
renaerdelyi <rena.szabo@yahoo.com>
2025-03-10 06:37:50 +0200

    add6

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import React, { Suspense } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Movies from './pages/Movies/Movies';
 import MovieDetails from './components/MovieDetails/MovieDetails';
-// import NotFoundPage from 'pages/common/NotFound/NotFoundPage';
 
 const Cast = React.lazy(() => import('./components/MovieDetails/Cast'));
 const Reviews = React.lazy(() => import('./components/MovieDetails/Reviews'));
-const NotFoundPage = React.lazy(() => import('./pages/common/NotFound/NotFoundPage'))
 
 const App = () => {
   return (
@@ -26,7 +20,6 @@ const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </Router>
